af2a8b400847301f1cc157abc00cb0e882d1db78
Nikola Kolarov <dzonik125@gmail.com>
2022-07-27 17:47:43 +0200

    First step of serialization

---
+++ src/main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+//		Adress adress = new Adress("Laze Kostica", 77, "Kovilj", 21243);
+//		Adress adress2 = new Adress("Krva Bikickog", 24, "Budisava", 21242);
+//		List<Adress> adresses = new ArrayList<Adress>();
+//		adresses.add(adress);
+//		adresses.add(adress2);
+//		
+//		Gson gson = new Gson();
+//		Writer writer = new FileWriter("C:\\Users\\dZoNi\\eclipseEnterprise-workspace\\PassFit\\data\\adress.json", true);
+//		gson.toJson(adresses, writer);
+//		writer.flush();
+//		writer.close();
+//		Primer dobre serijalizcije (Neophodno prvo dodati bazu koja ce cuvati liste podataka iz modela)!
+	}
+
+}
+	
